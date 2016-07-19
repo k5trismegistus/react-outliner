d9e9d7f0e15a03b96320a0b3be80cd624aa795bd
keigo_yamamoto <keigo_yamamoto@dwango.co.jp>
2016-07-19 13:17:54 +0900

    initial commit

---
+++ public/src/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    hello
+  </div>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'example': './public/src/index.js',
+    },
+    output: {
+        path: './public/dist/',
+        filename: 'index.js'
+    },
+    devtool: 'source-map',
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=react,presets[]=es2015'
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader?presets[]=react,presets[]=es2015'
+            }
+        ]
+    },
+    resolve: {
+    extensions: ['', '.js', '.jsx'],
+  }
+};
