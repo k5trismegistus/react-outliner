15257b4e38627214699d2b7929ccd68747918402
keigo_yamamoto <keigo_yamamoto@dwango.co.jp>
2016-07-25 14:50:53 +0900

    使用するポートを変更

--- server.py
+++ server.py
@@ -18,4 +18,4 @@ app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 3000)))
+    app.run(port=int(os.environ.get("PORT", 4000)))
