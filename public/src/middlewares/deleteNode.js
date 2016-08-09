2b3dbbce37f78c178ce0db1788b670584da7f66b
Keigo <k5.trismegistus@gmail.com>
2016-08-09 22:45:02 +0900

    子供がいるノードは削除できない

--- public/src/middlewares/deleteNode.js
+++ public/src/middlewares/deleteNode.js
@@ -4,9 +4,22 @@ import { DELETE_NODE } from '../actions/compositeActions'
 import { removeNode } from '../actions/nodes'
 import { removeRelation, unregisterRelation } from '../actions/relations'
 
+const findRelationById = (relations, nodeId) => {
+  return (relations.find(r => {
+    return (r.id === nodeId)
+  }))
+}
+
 export const mwDeleteNode = store => next => action => {
   if (action.type != DELETE_NODE) {
     next(action)
+    return
+  }
+
+  const relations = store.getState().relations.relations
+  const hasChildren = (findRelationById(relations, action.payload.nodeId).childrenIds.length > 0)
+
+  if (hasChildren) {
     return
   }
 
